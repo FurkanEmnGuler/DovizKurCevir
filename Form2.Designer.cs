e23142c1d6073dada80588c00ea64fe654ccf6c8
Rophe1 <gulerfurkanemin@gmail.com>
2024-09-03 00:10:10 +0300

    Initial commit

---
+++ Form2.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace DovizKurCevir
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtEURO = new System.Windows.Forms.TextBox();
+            this.txtUSD = new System.Windows.Forms.TextBox();
+            this.txtTL = new System.Windows.Forms.TextBox();
+            this.labelTL = new System.Windows.Forms.Label();
+            this.labelEURO = new System.Windows.Forms.Label();
+            this.labelUSD = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtEURO
+            // 
+            this.txtEURO.Location = new System.Drawing.Point(678, 34);
+            this.txtEURO.Multiline = true;
+            this.txtEURO.Name = "txtEURO";
+            this.txtEURO.Size = new System.Drawing.Size(100, 33);
+            this.txtEURO.TabIndex = 33;
+            // 
+            // txtUSD
+            // 
+            this.txtUSD.Location = new System.Drawing.Point(403, 34);
+            this.txtUSD.Multiline = true;
+            this.txtUSD.Name = "txtUSD";
+            this.txtUSD.Size = new System.Drawing.Size(100, 33);
+            this.txtUSD.TabIndex = 32;
+            // 
+            // txtTL
+            // 
+            this.txtTL.Location = new System.Drawing.Point(129, 34);
+            this.txtTL.Multiline = true;
+            this.txtTL.Name = "txtTL";
+            this.txtTL.Size = new System.Drawing.Size(100, 33);
+            this.txtTL.TabIndex = 28;
+            // 
+            // labelTL
+            // 
+            this.labelTL.AutoSize = true;
+            this.labelTL.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelTL.Location = new System.Drawing.Point(52, 34);
+            this.labelTL.Name = "labelTL";
+            this.labelTL.Size = new System.Drawing.Size(49, 33);
+            this.labelTL.TabIndex = 26;
+            this.labelTL.Text = "TL";
+            // 
+            // labelEURO
+            // 
+            this.labelEURO.AutoSize = true;
+            this.labelEURO.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelEURO.Location = new System.Drawing.Point(552, 34);
+            this.labelEURO.Name = "labelEURO";
+            this.labelEURO.Size = new System.Drawing.Size(99, 33);
+            this.labelEURO.TabIndex = 25;
+            this.labelEURO.Text = "EURO";
+            // 
+            // labelUSD
+            // 
+            this.labelUSD.AutoSize = true;
+            this.labelUSD.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelUSD.Location = new System.Drawing.Point(287, 34);
+            this.labelUSD.Name = "labelUSD";
+            this.labelUSD.Size = new System.Drawing.Size(76, 33);
+            this.labelUSD.TabIndex = 24;
+            this.labelUSD.Text = "USD";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(129, 141);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 33);
+            this.textBox1.TabIndex = 35;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(52, 141);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 33);
+            this.label1.TabIndex = 34;
+            this.label1.Text = "TL";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(129, 272);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 33);
+            this.textBox2.TabIndex = 39;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(129, 208);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 33);
+            this.textBox3.TabIndex = 38;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(3, 272);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 33);
+            this.label2.TabIndex = 37;
+            this.label2.Text = "EURO";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(13, 208);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 33);
+            this.label3.TabIndex = 36;
+            this.label3.Text = "USD";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtEURO);
+            this.Controls.Add(this.txtUSD);
+            this.Controls.Add(this.txtTL);
+            this.Controls.Add(this.labelTL);
+            this.Controls.Add(this.labelEURO);
+            this.Controls.Add(this.labelUSD);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtEURO;
+        private System.Windows.Forms.TextBox txtUSD;
+        private System.Windows.Forms.TextBox txtTL;
+        private System.Windows.Forms.Label labelTL;
+        private System.Windows.Forms.Label labelEURO;
+        private System.Windows.Forms.Label labelUSD;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
